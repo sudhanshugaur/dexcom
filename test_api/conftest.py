cdbdf15c9ea5b8a3753bb1f826ae37d71802f15e
Sudhanshu Gaur <sudhanshugaur@Sudhanshus-MacBook-Pro.local>
2022-12-06 21:20:03 -0800

    modifications

--- test_api/conftest.py
+++ test_api/conftest.py
@@ -2,13 +2,11 @@
 import pytest
 import os.path
 
-
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     # execute all other hooks to obtain the report object
     outcome = yield
     rep = outcome.get_result()
-
     # we only look at actual failing test calls, not setup/teardown
     if rep.when == "call" and rep.failed:
         mode = "a" if os.path.exists("failures.txt") else "w"
